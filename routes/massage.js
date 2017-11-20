c217bd0b410fea0629fcc931e03fa4715d006bd2
m1245 <m1245@DESKTOP-VVLE821>
2017-11-20 20:36:34 +0200

    Merge branch 'master' of https://github.com/Galit1321/Web_xss
    # Conflicts:
    #	views/massage.jade

--- routes/massage.js
+++ routes/massage.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+app.get("/login",function(req,res) {
+    var param = req.query["user_name"];
+    console.log(param);
+});
 
 function redirect(res){
     window.location = res;
