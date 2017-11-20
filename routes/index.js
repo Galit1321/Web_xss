b39b0087bf8cc6b06f03a463b96fff0ef73e2b04
ifat neumann <ineumann19@gmail.com>
2017-11-20 13:06:10 +0200

    galit

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,21 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/login',function(req, res) {
-    var dict = []; // create an empty array
-
+    var personDict = []; // create an empty array
+    var postsDict = []; // create an empty array
     var person=[{
         userName:req.body.user_name ,
         pass:req.body.password
     }
     ]
-
-    dict.push(person);
-    res.render('massage', {user: req.body.user_name , people: person});
+    var firstPost=[{
+        title: "first response!",
+        text: "hi there"
+    }
+    ]
+    personDict.push(person);
+    postsDict.push(firstPost);
+    res.render('massage', {user: req.body.user_name , people: person, pagePosts: postsDict});
 });
 
 
